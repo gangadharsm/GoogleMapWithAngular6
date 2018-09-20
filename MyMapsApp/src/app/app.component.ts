2a878e670abd0d506c7f4c725e5971d154b01efb
Gangadhara <gangadhara@bootminds.com>
2018-09-20 11:55:18 +0530

    map implemented

--- MyMapsApp/src/app/app.component.ts
+++ MyMapsApp/src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'My first AGM project';
+  lat: number;
+  lng: number;
+  zoom: number = 16;
+  constructor() {
+    this.findMe();
+  }
+  findMe() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position)
+        this.lat = position.coords.latitude;
+        this.lng = position.coords.longitude;
+      });
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
 }
--- MyMapsApp/src/app/app.module.ts
+++ MyMapsApp/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AgmCoreModule } from '@agm/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, 
+    ReactiveFormsModule,
+    AgmCoreModule.forRoot({
+      // please get your own API key here:
+      // https://developers.google.com/maps/documentation/javascript/get-api-key?hl=en
+      apiKey: 'AIzaSyDmlA2BCI7Xge_qpSQHJthu-TDX5STvF-4'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
